5887c857bb0f58228335ebb877602b5e5dd9f183
seanmckee <seanmingmckee@gmail.com>
2023-07-20 07:10:56 -0700

    toggle comment view

--- client/src/components/Tweet.tsx
+++ client/src/components/Tweet.tsx
@@ -22,6 +22,7 @@ const Tweet = ({ username, text, likes, liked, postID }: TweetProps) => {
   const [isLiked, setIsLiked] = useState(liked);
   const [tweetText, setTweetText] = useState("");
   const [comments, setComments] = useState([]);
+  const [commentsVisible, setCommentsVisible] = useState(false);
 
   const togglePopup = () => {
     setOpen(!open);
@@ -91,7 +92,6 @@ const Tweet = ({ username, text, likes, liked, postID }: TweetProps) => {
         `http://localhost:8000/posts/comments/${postID}`
       );
       setComments(response.data);
-      console.log(response.data);
     } catch (error) {
       console.error(error);
     }
@@ -99,71 +99,96 @@ const Tweet = ({ username, text, likes, liked, postID }: TweetProps) => {
 
   useEffect(() => {
     getComments();
-    console.log("running");
   }, []);
 
   return (
-    <div className="border-[1px] border-zinc-800 p-6">
-      <h1 className="text-white font-bold text-xl text-left w-full mb-0">
-        @{username}
-      </h1>
-      <p className="text-white">{text}</p>
-      <div className="text-white flex">
-        <div className="flex">
-          <button onClick={isLiked ? unlikeTweet : likeTweet}>
-            {isLiked ? (
-              <AiFillHeart style={{ color: "#e85f5f" }} />
-            ) : (
-              <AiOutlineHeart />
-            )}
-          </button>
-
-          <span className="ml-1">{currentLikes}</span>
-        </div>
-        <div>
-          <button className="ml-3">
-            <FaRegComment />
-          </button>
-          <span className="ml-1">{comments.length}</span>
-        </div>
-
-        <button onClick={togglePopup}>
-          <HiReply />
-        </button>
-        <Popup
-          open={open}
-          closeOnDocumentClick
-          // onClose={togglePopup}
-          contentStyle={{
-            background: "black",
-            position: "relative",
-            borderRadius: "2rem",
-          }}
-          overlayStyle={{
-            background:
-              "linear-gradient(rgba(147, 197, 253, 0.2), rgba(147, 197, 253, 0.2))",
-          }}
-        >
-          <div className="w-[500px] h-[250px] p-4 rounded-3xl">
-            <div className="absolute inset-0 bg-black rounded-3xl text-slate-400" />
-            <textarea
-              className="absolute focus:outline-0 border-0 top-4 bg-black border-t-0 mb-2 text-white resize-none w-[450px] h-[100px] placeholder:text-xl p-3 text-xl"
-              name="tweetText"
-              id="tweetText"
-              value={tweetText}
-              onChange={(event) => setTweetText(event.target.value)}
-              cols={20}
-              rows={10}
-              placeholder="Tweet your reply!"
-            ></textarea>
-            <button
-              onClick={postComment}
-              className="bg-blue-400 rounded-full text-white p-2 px-4 float-right absolute right-4 bottom-4"
-            >
-              Reply
+    <div>
+      <div className="border-[1px] border-zinc-800 p-6">
+        <h1 className="text-white font-bold text-xl text-left w-full mb-0">
+          @{username}
+        </h1>
+        <p className="text-white">{text}</p>
+        <div className="text-white flex">
+          <div className="flex">
+            <button onClick={isLiked ? unlikeTweet : likeTweet}>
+              {isLiked ? (
+                <AiFillHeart style={{ color: "#e85f5f" }} />
+              ) : (
+                <AiOutlineHeart />
+              )}
             </button>
+
+            <span className="ml-1">{currentLikes}</span>
           </div>
-        </Popup>
+          <div>
+            <button
+              className="ml-3"
+              onClick={() => setCommentsVisible(!commentsVisible)}
+            >
+              <FaRegComment />
+            </button>
+            <span className="ml-1">{comments.length}</span>
+          </div>
+
+          <button onClick={togglePopup}>
+            <HiReply />
+          </button>
+
+          <Popup
+            open={open}
+            closeOnDocumentClick
+            // onClose={togglePopup}
+            contentStyle={{
+              background: "black",
+              position: "relative",
+              borderRadius: "2rem",
+            }}
+            overlayStyle={{
+              background:
+                "linear-gradient(rgba(147, 197, 253, 0.2), rgba(147, 197, 253, 0.2))",
+            }}
+          >
+            <div className="w-[500px] h-[250px] p-4 rounded-3xl">
+              <div className="absolute inset-0 bg-black rounded-3xl text-slate-400" />
+              <textarea
+                className="absolute focus:outline-0 border-0 top-4 bg-black border-t-0 mb-2 text-white resize-none w-[450px] h-[100px] placeholder:text-xl p-3 text-xl"
+                name="tweetText"
+                id="tweetText"
+                value={tweetText}
+                onChange={(event) => setTweetText(event.target.value)}
+                cols={20}
+                rows={10}
+                placeholder="Tweet your reply!"
+              ></textarea>
+              <button
+                onClick={postComment}
+                className="bg-blue-400 rounded-full text-white p-2 px-4 float-right absolute right-4 bottom-4"
+              >
+                Reply
+              </button>
+            </div>
+          </Popup>
+        </div>
+      </div>
+      <div className={commentsVisible ? "w-[80%] m-auto" : "hidden"}>
+        {comments &&
+          comments
+            .slice(0)
+            .reverse()
+            .map((comment: any) => (
+              <Tweet
+                username={comment.username}
+                text={comment.text}
+                likes={comment.likes ? comment.likes.length : 0}
+                liked={
+                  comment.likes
+                    ? comment.likes.includes(localStorage.getItem("userID"))
+                    : false
+                }
+                postID={comment._id}
+                key={comment._id}
+              />
+            ))}
       </div>
     </div>
   );
